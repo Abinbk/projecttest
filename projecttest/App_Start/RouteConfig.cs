e9206aa989db6d6744dd8778f44caf3bb8faf132
Abin1997benny@gmail.com <Abin1997benny@gmail.com>
2025-11-16 21:30:11 +0530

    test gg

--- projecttest/App_Start/RouteConfig.cs
+++ projecttest/App_Start/RouteConfig.cs
@@ -10,7 +10,7 @@ namespace projecttest
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
-            var settings = new FriendlyUrlSettings();
+            var settings = new FriendlyUrlSettings();    
             settings.AutoRedirectMode = RedirectMode.Permanent;
             routes.EnableFriendlyUrls(settings);
         }
